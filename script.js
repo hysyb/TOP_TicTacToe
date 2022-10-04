052929bd3c324e440cc36885db3da343f24bb340
Jesse <jessejkilback@gmail.com>
2022-10-03 19:12:09 -0700

    initial commit and layout change

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 let ticTacToe = ["x","o","x","o","x","o","x","o","x"]
 
-let grid = document.querySelector('.grid');
-function drawGrid(){
-    for (i=0;i<ticTacToe.length;i++){
-        let newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.textContent = ticTacToe[i];
-        grid.appendChild(newSquare);
+//let square0 = document.getElementById('0');
+//square0.addEventListener('click', function(e) {gameLoop(this.id)})
+
+let squares = document.querySelectorAll('.square');
+let turn = 0;
+
+for (i=0;i<squares.length;i++){
+    squares[i].addEventListener('click', function(e) { gameLoop(this.id)});
+};
+
+function gameLoop(id) {
+    if (checkwin == true){
+        console.log(ticTacToe[id] + 'wins');
     }
-}
-function resetGrid(){
-    while (grid.firstChild){
-        grid.removeChild(grid.firstChild);
-    }
-    for (i=0;i<ticTacToe.length;i++){
-        ticTacToe[i] = "";
-    }
-    drawGrid();
-}
-function player(name, sign){
-    return {name, sign};
+    if (squares[id].innerText == ""){
+        squares[id].innerText = ticTacToe[turn];
+        turn=turn+1;
+    } 
 }
 
-drawGrid();
+function checkwin(){
+        if (squares[0].innerText == 'x' && squares[1].innerText == 'x' && squares[2].innerText == 'x'){
+            return true;
+        }
+        else return false;
+}
