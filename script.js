e54e27abb5e3469a779870a1139be3ae16221e8f
Jesse <jessejkilback@gmail.com>
2022-10-03 21:41:27 -0700

    add click ufnctionality to draw x and os

--- script.js
+++ script.js
@@ -5,24 +5,31 @@ let ticTacToe = ["x","o","x","o","x","o","x","o","x"]
 
 let squares = document.querySelectorAll('.square');
 let turn = 0;
+let circleTurn = false;
 
 for (i=0;i<squares.length;i++){
     squares[i].addEventListener('click', function(e) { gameLoop(this.id)});
 };
 
 function gameLoop(id) {
-    if (checkwin == true){
-        console.log(ticTacToe[id] + 'wins');
-    }
     if (squares[id].innerText == ""){
-        squares[id].innerText = ticTacToe[turn];
-        turn=turn+1;
+        if (circleTurn){
+            squares[id].innerText = 'o';
+        }else{
+            squares[id].innerText = 'x';
+        }
+        circleTurn = !circleTurn;
     } 
+    if (checkwin()){
+        console.log(`${circleTurn ? 'o ' : 'x '}` + 'wins');
+    }
 }
-
 function checkwin(){
-        if (squares[0].innerText == 'x' && squares[1].innerText == 'x' && squares[2].innerText == 'x'){
+        if (squares[0].innerHTML == 'x' && squares[1].innerHTML == 'x' && squares[2].innerHTML == 'x'){
             return true;
         }
         else return false;
+}
+function checkSquare(id){
+    console.log(squares[id].innerHTML);
 }
