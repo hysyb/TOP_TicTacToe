06b33f7d9c6bb5c341bfc1be3b6cb8e9cc51655c
Jesse <jessejkilback@gmail.com>
2022-10-11 20:34:05 -0700

    add computer functionality and selector radios

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ let winScreenX = document.getElementById('Xwin');
 let winScreenO = document.getElementById('Owin');
 let drawScreen = document.getElementById('Draw');
 let restartBtn = document.querySelectorAll('.restartButton');
+let pvpRadio = document.getElementById('pvp');
+let pvcRadio = document.getElementById('pvc');
+let player2 = 'human';
+let playAs = 'x';
+
 
 for (i=0;i<squares.length;i++){
     squares[i].addEventListener('click', function(e) { gameLoop(this.id)});
@@ -12,16 +17,35 @@ for (i=0;i<squares.length;i++){
 for (i=0;i<restartBtn.length;i++){
 restartBtn[i].addEventListener('click', ()=>reset());
 }
+pvpRadio.addEventListener('click', () => player2 = 'human');
+pvcRadio.addEventListener('click', () => player2 = 'computer');
 
 function gameLoop(id) {
-    
-    if (squares[id].innerText == ""){
+    if (player2 == 'computer'){
+        if (squares[id].innerText == ""){
+                squares[id].innerText = playAs;
+                checkwinner();
+            }
+        for (i=0;i<squares.length;i++){
+            if (squares[i].innerText == ""){
+                squares[i].innerText = 'o';
+                checkwinner;
+                return;
+            }
+        }
+    }else if (squares[id].innerText == ""){
         if (circleTurn){
             squares[id].innerText = 'o';
         }else{
             squares[id].innerText = 'x';
         }
+        circleTurn = !circleTurn;
     } 
+    checkwinner();
+    turn++;
+}
+//win condition checker
+function checkwinner(){
     if (checkwin('x')){
         console.log('X wins');
         winScreenX.classList.remove('hide');
@@ -30,12 +54,10 @@ function gameLoop(id) {
     }else if (checkwin('o')){
         console.log('O wins')
         winScreenO.classList.remove('hide');
-    }else if (turn == 8){
+    }else if (turn == 9){
         drawScreen.classList.remove('hide');
         return;
     }
-    circleTurn = !circleTurn;
-    turn++;
 }
 function checkwin(sign){
         if (squares[0].innerHTML == sign && squares[1].innerHTML == sign && squares[2].innerHTML == sign){
@@ -57,6 +79,7 @@ function checkwin(sign){
         }
         else return false;
 }
+//clear gameboard
 function reset(){
     for (i=0;i<squares.length;i++){
         squares[i].innerHTML = '';
